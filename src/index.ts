9eb52e66c26c424149029c8311b42c44df130cd6
Richard Tibbett <richard.tibbett@whereby.com>
2025-10-07 16:21:16 +0200

    Update to latest Assistant SDK API

--- src/index.ts
+++ src/index.ts
@@ -4,15 +4,7 @@ import fs from "fs";
 import { spawn } from "child_process";
 
 import "dotenv/config";
-import {
-    Trigger,
-    Assistant,
-    TRIGGER_EVENT_SUCCESS,
-    ASSISTANT_JOINED_ROOM,
-    ASSISTANT_LEFT_ROOM,
-    AUDIO_STREAM_READY,
-    AudioSink,
-} from "@whereby.com/assistant-sdk";
+import { Trigger, Assistant, TRIGGER_EVENT_SUCCESS, ASSISTANT_LEFT_ROOM } from "@whereby.com/assistant-sdk";
 import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
 
 const roomState: Record<string, boolean> = {};
@@ -30,7 +22,7 @@ const trigger = new Trigger({
 
 trigger.on(
     TRIGGER_EVENT_SUCCESS,
-    ({
+    async ({
         roomUrl,
         triggerWebhook: {
             data: { subdomain, roomName },
@@ -40,119 +32,118 @@ trigger.on(
 
         const assistant = new Assistant({
             assistantKey: process.env.WHEREBY_ASSISTANT_KEY ?? "",
-            startCombinedAudioStream: true,
-            startLocalMedia: false,
         });
 
-        assistant.joinRoom(roomUrl);
+        try {
+            await assistant.joinRoom(roomUrl);
+        } catch (error) {
+            console.log(`An error occurred connecting to ${roomUrl}`, error);
+            return;
+        }
 
-        assistant.on(ASSISTANT_JOINED_ROOM, () => {
-            console.log("Assistant joined the room");
+        console.log("Assistant joined the room");
 
-            const roomStateKey = `${subdomain}${roomName}`;
-            roomState[roomStateKey] = true;
+        const roomStateKey = `${subdomain}${roomName}`;
+        roomState[roomStateKey] = true;
 
-            const startTimestamp = new Date().toISOString();
+        const startTimestamp = new Date().toISOString();
 
-            // prettier-ignore
-            const ffmpegProcess = spawn("ffmpeg", [
-                "-hide_banner",
-                "-nostats",
-                "-loglevel", "verbose",
-                "-y",
-                "-vn",
-                "-acodec", "pcm_s16le",
-                "-f", "s16le",
-                "-ac", "1",
-                "-ar", "48000",
-                "-i", "pipe:0",
-                `/tmp/audiorecorder-${startTimestamp}.mp3`,
-            ]);
+        // prettier-ignore
+        const ffmpegProcess = spawn("ffmpeg", [
+            "-hide_banner",
+            "-nostats",
+            "-loglevel", "verbose",
+            "-y",
+            "-vn",
+            "-acodec", "pcm_s16le",
+            "-f", "s16le",
+            "-ac", "1",
+            "-ar", "48000",
+            "-i", "pipe:0",
+            `/tmp/audiorecorder-${startTimestamp}.mp3`,
+        ]);
 
-            assistant.on(AUDIO_STREAM_READY, ({ track }) => {
-                const audioSink = new AudioSink(track);
+        const combinedAudioSink = assistant.getCombinedAudioSink();
 
-                const unsubscribeAudioSink = audioSink.subscribe(({ samples }) => {
-                    if (!ffmpegProcess.stdin.writable) {
-                        unsubscribeAudioSink();
-                        return;
-                    }
-
-                    ffmpegProcess.stdin.write(samples);
-                });
-
-                console.log("Started recording audio...");
-
-                ffmpegProcess.on("close", () => {
-                    if (process.env.AWS_S3_ACCESS_KEY_ID && process.env.AWS_S3_SECRET_ACCESS_KEY) {
-                        console.log("Uploading audio output to S3...");
-
-                        const s3OutputFileName = `${roomStateKey}/${startTimestamp}.mp3`;
-
-                        const s3Client = new S3Client({
-                            credentials: {
-                                accessKeyId: process.env.AWS_S3_ACCESS_KEY_ID as string,
-                                secretAccessKey: process.env.AWS_S3_SECRET_ACCESS_KEY as string,
-                            },
-                            region: process.env.AWS_S3_REGION,
-                        });
-
-                        fs.readFile(`/tmp/audiorecorder-${startTimestamp}.mp3`, (err, data) => {
-                            if (err) {
-                                throw err;
-                            }
-
-                            s3Client
-                                .send(
-                                    new PutObjectCommand({
-                                        Bucket: process.env.AWS_S3_BUCKET_NAME,
-                                        Key: s3OutputFileName,
-                                        Body: data,
-                                    }),
-                                )
-                                .then(() => {
-                                    console.log(
-                                        `Successfully uploaded audio output to S3 @ ${process.env.AWS_S3_BUCKET_NAME}:${s3OutputFileName}`,
-                                    );
-                                })
-                                .catch((error) => {
-                                    console.error("An error occured uploading to S3.", error);
-                                });
-                        });
-                    }
-
-                    console.log(`Audio recording completed for ${roomUrl}`);
-                });
-            });
-
-            assistant.on(ASSISTANT_LEFT_ROOM, () => {
-                console.log("Assistant left the room");
-                roomState[roomStateKey] = false;
-
-                if (ffmpegProcess) {
-                    ffmpegProcess.stdin.end();
+        if (combinedAudioSink) {
+            const unsubscribeAudioSink = combinedAudioSink.subscribe(({ samples }) => {
+                if (!ffmpegProcess.stdin.writable) {
+                    unsubscribeAudioSink();
+                    return;
                 }
+
+                ffmpegProcess.stdin.write(samples);
             });
+        }
 
-            const roomConnection = assistant.getRoomConnection();
+        console.log("Started recording audio...");
 
-            // If less than 2 human participants remain in a room, stop recording audio
-            const unsubscribeFromRemoteParticipants = roomConnection.subscribeToRemoteParticipants(
-                (remoteParticipants) => {
-                    const humanParticipants = remoteParticipants.filter(({ roleName }) =>
-                        ["owner", "member", "host", "visitor", "granted_visitor", "viewer", "granted_viewer"].includes(
-                            roleName,
-                        ),
-                    );
+        ffmpegProcess.on("close", () => {
+            if (process.env.AWS_S3_ACCESS_KEY_ID && process.env.AWS_S3_SECRET_ACCESS_KEY) {
+                console.log("Uploading audio output to S3...");
 
-                    if (humanParticipants.length <= 1) {
-                        unsubscribeFromRemoteParticipants();
+                const s3OutputFileName = `${roomStateKey}/${startTimestamp}.mp3`;
 
-                        console.log("Assistant leaving the room");
-                        roomConnection.leaveRoom();
+                const s3Client = new S3Client({
+                    credentials: {
+                        accessKeyId: process.env.AWS_S3_ACCESS_KEY_ID as string,
+                        secretAccessKey: process.env.AWS_S3_SECRET_ACCESS_KEY as string,
+                    },
+                    region: process.env.AWS_S3_REGION,
+                });
+
+                fs.readFile(`/tmp/audiorecorder-${startTimestamp}.mp3`, (err, data) => {
+                    if (err) {
+                        throw err;
                     }
-                },
+
+                    s3Client
+                        .send(
+                            new PutObjectCommand({
+                                Bucket: process.env.AWS_S3_BUCKET_NAME,
+                                Key: s3OutputFileName,
+                                Body: data,
+                            }),
+                        )
+                        .then(() => {
+                            console.log(
+                                `Successfully uploaded audio output to S3 @ ${process.env.AWS_S3_BUCKET_NAME}:${s3OutputFileName}`,
+                            );
+                        })
+                        .catch((error) => {
+                            console.error("An error occured uploading to S3.", error);
+                        });
+                });
+            }
+
+            console.log(`Audio recording completed for ${roomUrl}`);
+        });
+
+        assistant.on(ASSISTANT_LEFT_ROOM, () => {
+            console.log("Assistant left the room");
+            roomState[roomStateKey] = false;
+
+            if (ffmpegProcess) {
+                ffmpegProcess.stdin.end();
+            }
+        });
+
+        const roomConnection = assistant.getRoomConnection();
+
+        // If less than 2 human participants remain in a room, stop recording audio
+        const unsubscribeFromRemoteParticipants = roomConnection.subscribeToRemoteParticipants((remoteParticipants) => {
+            const humanParticipants = remoteParticipants.filter(({ roleName }) =>
+                ["owner", "member", "host", "visitor", "granted_visitor", "viewer", "granted_viewer"].includes(
+                    roleName,
+                ),
             );
+
+            if (humanParticipants.length <= 1) {
+                unsubscribeFromRemoteParticipants();
+
+                console.log("Assistant leaving the room");
+                roomConnection.leaveRoom();
+            }
         });
     },
 );
